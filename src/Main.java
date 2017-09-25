c32e3ca106ac15bb5ffb68dc7648026e055e06d4
賴品宇 <fals0827@gmail.com>
2017-09-25 15:59:38 +0800

    do

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// id:106021048
+// name:賴品宇
+// date:2017 09 25
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        float l = scn.nextFloat();
+        System.out.println(Math.round(l*0.26418f*10f)/10f);
+    }
+}
